c722ed04c6f66ea132c63bc23519432d19827a0b
schang1146 <sammychang2185@gmail.com>
2019-06-27 17:59:13 -0700

    added Login.js and PrivateRoute.js

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,13 +1,26 @@
 import axios from 'axios';
 
+export const LOGIN_START = "LOGIN_START";
 export const FETCH_FRIENDS_START = "FETCH_FRIENDS_START";
 export const FETCH_FRIENDS_SUCCESS = "FETCH_FRIENDS_SUCCESS";
 export const FETCH_FRIENDS_FAILURE = "FETCH_FRIENDS_FAILURE";
 
+export const login = creds => dispatch => {
+    dispatch({ type: LOGIN_START });
+    return axios
+        .post('http://localhost:5000/api/login')
+        .then(res => localStorage.setItem('token', res.data.payload))
+}
+
 export const getFriendsList = () => dispatch => {
     dispatch({ type: FETCH_FRIENDS_START });
     axios
-        .get('http://localhost:5000')
+        .get('http://localhost:5000/api/friends')
         .then(res => dispatch({ type: FETCH_FRIENDS_SUCCESS, payload: res }))
-        .catch(err => dispatch({ type: FETCH_FRIENDS_FAILURE, payload: err }))
+        .catch(err => {
+            if (err.response.status === 403) {
+                localStorage.removeItem('token');
+            }
+            dispatch({ type: FETCH_FRIENDS_FAILURE, payload: err })
+        });
 };
---
+++ friends/src/components/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { login } from '../actions';
+
+class Login extends Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    handleLogin = e => {
+        e.preventDefault();
+        this.props
+            .login(this.state.credentials)
+            .then(() => this.props.history.push('/friends'));
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleLogin}>
+                    <input
+                        type='text'
+                        name='username'
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Log In</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default connect(
+    null,
+    { login }
+)(Login);
